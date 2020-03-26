ab5f37941cff11caaf967035d5f72df13401e403
maxie7 <maxie7@protonmail.com>
2020-03-26 15:30:46 +0100

    Add >> stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_dtX41D65QMsot2qLSm9O8NJR003ldkqkHf';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      currency='EUR'
+      label='Pay Now'
+      name='CRWN Clothing'
+      billingAddress
+      shippingAddress
+      image='http://svgshare.com/i/CUz.svg'
+      description={`Your total is ${price}€`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+};
+
+export default StripeCheckoutButton;
