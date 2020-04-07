307b6377850546309bc93c17b59446b2287f0558
maxie7 <maxie7@protonmail.com>
2020-04-07 15:22:31 +0200

    Refactor session >> convert components scss to styled: collections-overview && directory components

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,17 +4,16 @@ import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
-import './collections-overview.styles.scss';
-
+import { CollectionsOverviewComponent } from './collections-overview.styles';
 
 const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
+  <CollectionsOverviewComponent>
     {
       collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))
     }
-  </div>
+  </CollectionsOverviewComponent>
 );
 
 const mapStateToProps = createStructuredSelector({
