6b3ebe631dadba3fd037cdef4ec68ee04377d4a8
maxie7 <maxie7@protonmail.com>
2020-04-15 17:56:21 +0200

    Refactor session >> from observer pattern to promise pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -27,11 +27,14 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot =  collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    })
+    // fetch('https://firestore.googleapis.com/v1/projects/crwn-db-93e27/databases/(default)/documents/collections')
+    //   .then(response => response.json())
+
+    collectionRef.get().then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
+        this.setState({ loading: false });
+    });
   }
 
   render() {
