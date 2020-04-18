e3832e850d15945a66aa622bd18dcd20ed3a7dbe
maxie7 <maxie7@protonmail.com>
2020-04-18 12:55:47 +0200

    Async Redux >> fix thunk 'when refresh page with collection'

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,7 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component';
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -21,14 +21,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionFetching } = this.props;
+    const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
     return (
       <div className='shop-page'>
         <Route exact path={`${match.path}`}
                render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} /> }
         />
         <Route path={`${match.path}/:collectionId`}
-               render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} /> }
+               render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} /> }
         />
       </div>
     );
@@ -36,7 +36,8 @@ class ShopPage extends React.Component {
 }
 
 const mapStateToProp = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching
+  isCollectionFetching: selectIsCollectionFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
