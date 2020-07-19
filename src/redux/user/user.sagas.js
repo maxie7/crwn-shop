2f976c7133ed1e8b3a4b94799f67a736be2f9034
maxie7 <maxie7@protonmail.com>
2020-07-19 21:26:12 +0200

    ADD >> Google SignIn into Sagas [save state]

---
+++ src/redux/user/user.sagas.js
@@ -0,0 +1,10 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import UserActionTypes from './user.types';
+
+export function* signInWithGoogle() {
+
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, )
+}
