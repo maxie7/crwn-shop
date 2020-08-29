2d381b49872ce41ee977dda9e425fc1074a54b62
maxie7 <maxie7@protonmail.com>
2020-08-29 14:09:59 +0200

    Clean up function for useEffect

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import {selectCollection} from '../../redux/shop/shop.selectors';
 
 import {
-  CollectionPageContainer ,
+  CollectionPageContainer,
   CollectionTitle,
   CollectionItemsContainer
 } from './collection.styles';
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+import {firestore} from '../../firebase/firebase.utils';
+
+const CollectionPage = ({collection}) => {
+  useEffect(() => {
+    console.log('...subscribing...');
+    const unsubscribeFromCollections = firestore
+      .collection('collections')
+      .onSnapshot(snapshot => console.log(snapshot));
+    return () => {
+      console.log('...unsubscribing...');
+      unsubscribeFromCollections();
+    }
+  }, []);
+
+  const {title, items} = collection;
   return (
     <CollectionPageContainer>
-      <CollectionTitle>{ title }</CollectionTitle>
+      <CollectionTitle>{title}</CollectionTitle>
       <CollectionItemsContainer>
-        { items.map( item => ( <CollectionItem key={item.id} item={item} />) ) }
+        {items.map(item => (<CollectionItem key={item.id} item={item}/>))}
       </CollectionItemsContainer>
     </CollectionPageContainer>
   );
